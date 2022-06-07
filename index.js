e130f8f05c6f140c79df5689787778410dae3640
final3blindside <final3blindside@gmail.com>
2022-06-08 01:13:40 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("Welcome to my portfolio, dear visitor!");
+var firstname = prompt("What is your first name?");
+
+if (firstname == "null") {
+    alert("Hello dear guest!");
+} else if (firstname) {
+    alert ("Hello" + firstname);
+} else {
+    alert("Hello guest!");
+}
