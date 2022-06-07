0ae58bf6d8eb691c06598c6ab6361a219ce992e3
final3blindside <final3blindside@gmail.com>
2022-06-08 01:19:37 +0800

    Js finally here!
    Today marks the first time Javascript is used for the site. I am happy.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var firstname = prompt("What is your first name?");
 if (firstname == "null") {
     alert("Hello dear guest!");
 } else if (firstname) {
-    alert ("Hello" + firstname);
+    alert ("Hello " + firstname + "!");
 } else {
     alert("Hello guest!");
 }
